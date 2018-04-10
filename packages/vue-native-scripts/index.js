97bb4d2c01d8d5585bb08b12f8a59d483fedd776
gauravprwl14 <gauravprwl14@gmail.com>
2018-04-10 17:10:49 +0530

    remove watch functionality from the compiler.js file of vue-native-scripts, and exporting compileVueToRn function

--- packages/vue-native-scripts/index.js
+++ packages/vue-native-scripts/index.js
@@ -1,1 +1,2 @@
-module.exports = require('./src/bin/vue-native-script.js');
+// module.exports = require('./src/bin/vue-native-script.js');
+module.exports = require('./src/scripts/compiler.js');
--- packages/vue-native-scripts/src/scripts/compiler.js
+++ packages/vue-native-scripts/src/scripts/compiler.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+// const fs = require('fs');
 const compiler = require('vue-native-template-compiler');
 const cssParse = require('css-parse');
 const beautify = require('js-beautify').js_beautify;
@@ -7,11 +7,11 @@ const addvm = require('../util/addvm');
 const parseCss = require('../util/parseCss');
 
 // the watch reference node-watch, there may be some changes in the future
-const watch = require('../util/watch');
+// const watch = require('../util/watch');
 
-const walk = require('../util/walk');
+// const walk = require('../util/walk');
 
-const FILTER = /\.vue$/;
+// const FILTER = /\.vue$/;
 
 const DEFAULT_OUTPUT = {
   template: {
@@ -21,135 +21,132 @@ const DEFAULT_OUTPUT = {
   script: `const ${constants.SCRIPT_OPTIONS} = {}`
 };
 
-walk('./', {
-  filter:  FILTER
-}, function (name) {
-  action(name);
-});
+// walk('./', {
+//   filter:  FILTER
+// }, function (name) {
+//   compileVueToRn(name);
+// });
 
-watch('./', {
-  recursive: true,
-  filter:  FILTER
-}, function (evt, name) {
-  if (evt === 'update') {
-    action(name);
-  } else if (evt === 'remove') {
-    remove(name);
+// watch('./', {
+//   recursive: true,
+//   filter:  FILTER
+// }, function (evt, name) {
+//   if (evt === 'update') {
+//     compileVueToRn(name);
+//   } else if (evt === 'remove') {
+//     remove(name);
+//   }
+// });
+
+function compileVueToRn(resource) {
+  const code = resource.toString();
+  const cparsed = compiler.parseComponent(code, { pad: 'line' });
+
+  // console.log(cparsed);
+
+  let output = '';
+
+  // add react-vue import
+  output += `import ${constants.VUE}, { observer as ${
+    constants.OBSERVER
+  } } from 'vue-native-core'`;
+  output += '\n';
+
+  // // add react import
+  // output += `import ${constants.REACT} from 'react'`
+  // output += '\n';
+
+  // add react-native import
+  output += `import ${constants.REACT_NATIVE} from 'react-native'`;
+  output += '\n';
+
+  // add prop-type import
+  output += `import ${constants.PROP_TYPE} from 'prop-types'`;
+  output += '\n';
+
+  // add component builder import
+  output += `import { buildNativeComponent as ${
+    constants.BUILD_COMPONENT
+  } } from 'vue-native-helper'`;
+  output += '\n';
+
+  // parse template
+  const template = cparsed.template;
+  let templateParsed = DEFAULT_OUTPUT.template;
+  if (template) {
+    const templateContent = template.content.replace(/\/\/\n/g, '').trim();
+    if (templateContent) {
+      templateParsed = parseTemplate(templateContent);
+    }
   }
-});
 
-function action (name) {
-  fs.readFile(name, function (err, resource) {
-    if (err) {
-      throw err;
-    } else {
-      const code = resource.toString();
-      const cparsed = compiler.parseComponent(code, { pad: 'line' });
+  // add render dep import
+  output += templateParsed.import;
+  output += '\n';
 
-      // console.log(cparsed);
-      
-      let output = '';
+  // parse script
+  const script = cparsed.script;
+  let scriptParsed = DEFAULT_OUTPUT.script;
+  if (script) {
+    const scriptContent = script.content.replace(/\/\/\n/g, '').trim();
+    scriptParsed = parseScript(scriptContent);
+  }
 
-      // add react-vue import
-      output += `import ${constants.VUE}, { observer as ${constants.OBSERVER} } from 'vue-native-core'`;
-      output += '\n';
+  // add vue options
+  output += scriptParsed;
+  output += '\n\n';
 
-      // // add react import
-      // output += `import ${constants.REACT} from 'react'`
-      // output += '\n';
+  // add render funtion
+  output += addvm(templateParsed.render);
+  output += '\n\n';
 
-      // add react-native import
-      output += `import ${constants.REACT_NATIVE} from 'react-native'`;
-      output += '\n';
-
-      // add prop-type import
-      output += `import ${constants.PROP_TYPE} from 'prop-types'`;
-      output += '\n';
-
-      // add component builder import
-      output += `import { buildNativeComponent as ${constants.BUILD_COMPONENT} } from 'vue-native-helper'`;
-      output += '\n';
-
-      // parse template
-      const template = cparsed.template;
-      let templateParsed = DEFAULT_OUTPUT.template;
-      if (template) {
-        const templateContent = template.content.replace(/\/\/\n/g, '').trim();
-        if (templateContent) {
-          templateParsed = parseTemplate(templateContent);
-        }
-      }
-
-      // add render dep import
-      output += templateParsed.import;
-      output += '\n';
-
-      // parse script
-      const script = cparsed.script;
-      let scriptParsed = DEFAULT_OUTPUT.script;
-      if (script) {
-        const scriptContent = script.content.replace(/\/\/\n/g, '').trim();
-        scriptParsed = parseScript(scriptContent);
-      }
-      
-      // add vue options
-      output += scriptParsed;
-      output += '\n\n';
-
-      // add render funtion
-      output += addvm(templateParsed.render);
-      output += '\n\n';
-
-      // parse css
-      const styles = cparsed.styles;
-      let cssParsed = {};
-      styles.forEach(function(v) {
-        const cssAst = cssParse(v.content);
-        cssParsed = Object.assign({}, cssParsed, parseCss(cssAst));
-      });
-
-      // add css obj
-      output += `const ${constants.CSS} = ${JSON.stringify(cssParsed)}`;
-      output += '\n\n';
-
-      // add builder
-      output += `const ${constants.COMPONENT_BUILDED} = ${constants.BUILD_COMPONENT}(${constants.TEMPLATE_RENDER}, ${constants.SCRIPT_OPTIONS}, {Component: ${
-        constants.COMPONENT
-      }, PropTypes: ${
-        constants.PROP_TYPE
-      }, Vue: ${
-        constants.VUE
-      }, ReactNative: ${
-        constants.REACT_NATIVE
-      }, css: ${
-        constants.CSS
-      }})`;
-      output += '\n\n';
-
-      // export default
-      output += `export default ${constants.OBSERVER}(${constants.COMPONENT_BUILDED})`;
-
-      // beautiful
-      output = beautify(output, { 'indent_size': 2 });
-
-      fs.writeFile(name.replace(FILTER, '.js'), output, function (err) {
-        if (err) {
-          throw err;
-        }
-      });
-    }
+  // parse css
+  const styles = cparsed.styles;
+  let cssParsed = {};
+  styles.forEach(function(v) {
+    const cssAst = cssParse(v.content);
+    cssParsed = Object.assign({}, cssParsed, parseCss(cssAst));
   });
+
+  // add css obj
+  output += `const ${constants.CSS} = ${JSON.stringify(cssParsed)}`;
+  output += '\n\n';
+
+  // add builder
+  output += `const ${constants.COMPONENT_BUILDED} = ${
+    constants.BUILD_COMPONENT
+  }(${constants.TEMPLATE_RENDER}, ${constants.SCRIPT_OPTIONS}, {Component: ${
+    constants.COMPONENT
+  }, PropTypes: ${constants.PROP_TYPE}, Vue: ${constants.VUE}, ReactNative: ${
+    constants.REACT_NATIVE
+  }, css: ${constants.CSS}})`;
+  output += '\n\n';
+
+  // export default
+  output += `export default ${constants.OBSERVER}(${
+    constants.COMPONENT_BUILDED
+  })`;
+
+  // beautiful
+  output = beautify(output, { indent_size: 2 });
+  return output;
+
+  // fs.writeFile(name.replace(FILTER, '.js'), output, function(err) {
+  //   if (err) {
+  //     throw err;
+  //   }
+  // });
 }
 
-function remove (name) {
-  fs.unlink(name.replace(FILTER, '.js'), function (err) {
-    if (err) {
-      throw err;
-    }
-  });
-}
+// function remove(name) {
+//   fs.unlink(name.replace(FILTER, '.js'), function(err) {
+//     if (err) {
+//       throw err;
+//     }
+//   });
+// }
 
-function parseTemplate (code) {
+function parseTemplate(code) {
   const obj = compiler.nativeCompiler(code);
   return {
     import: obj.importCode,
@@ -157,10 +154,12 @@ function parseTemplate (code) {
   };
 }
 
-function parseScript (code) {
+function parseScript(code) {
   const s = `const ${constants.SCRIPT_OPTIONS} = `;
   code = code
     .replace(/[\s;]*module.exports[\s]*=/, `\n${s}`)
     .replace(/[\s;]*export[\s]+default[\s]*\{/, `\n${s} {`);
   return code;
 }
+
+module.exports = compileVueToRn;
