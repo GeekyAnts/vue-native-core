af3a3cc5bc56f1b12b3eca37bcf58003fc23ea4e
gauravprwl14 <gauravprwl14@gmail.com>
2018-06-15 01:04:04 +0530

    rename the react-vue-helper to vue-native-helper

--- src/platforms/vue-native/compiler/constants.js
+++ src/platforms/vue-native/compiler/constants.js
@@ -3,7 +3,7 @@ const CREATE_ELEMENT = "createElement";
 const COMPONENT = "Component";
 const HELPER_HEADER = "__react__vue__";
 const HELPER_HEADER_NATIVE = "__react__vue__native__";
-const RENDER_HELPER_MODULE_NAME = "react-vue-helper";
+const RENDER_HELPER_MODULE_NAME = "vue-native-helper";
 
 export {
   TAB_INDENT,
