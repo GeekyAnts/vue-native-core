036119b13453bb54e6f49017adca1e896e225162
gauravprwl14 <gauravprwl14@gmail.com>
2018-06-16 23:30:10 +0530

    fix the linting error in the directive file

--- src/platforms/vue-native/runtime/render-helpers/directive.js
+++ src/platforms/vue-native/runtime/render-helpers/directive.js
@@ -1,19 +1,18 @@
-import {
-  COMMON
-} from 'react-vue/compiler/config'
+import { COMMON } from "react-vue/compiler/config";
 
-function triggerDirective (newData, oldData, vm, ref) {
-  let directive
+function triggerDirective(newData, oldData, vm, ref) {
+  let directive;
   if (newData) {
-    directive = vm.$options.directives[newData.name]
+    directive = vm.$options.directives[newData.name];
   } else if (oldData) {
-    directive = vm.$options.directives[oldData.name]
+    directive = vm.$options.directives[oldData.name];
   }
   if (!directive) {
-    return
+    return;
   }
-  let binding
-  if (newData && oldData) { // update
+  let binding;
+  if (newData && oldData) {
+    // update
     binding = {
       name: newData.name,
       value: newData.value,
@@ -21,146 +20,166 @@ function triggerDirective (newData, oldData, vm, ref) {
       expression: newData.expression,
       arg: newData.arg,
       modifiers: newData.modifiers
+    };
+    const args = [ref, binding];
+    if (typeof directive === "function") {
+      directive.apply(vm, args);
+    } else if (typeof directive.update === "function") {
+      directive.update.apply(vm, args);
     }
-    const args = [ref, binding]
-    if (typeof directive === 'function') {
-      directive.apply(vm, args)
-    } else if (typeof directive.update === 'function') {
-      directive.update.apply(vm, args)
-    }
-  } else if (newData && !oldData) { // bind
+  } else if (newData && !oldData) {
+    // bind
     binding = {
       name: newData.name,
       value: newData.value,
       expression: newData.expression,
       arg: newData.arg,
       modifiers: newData.modifiers
+    };
+    const args = [ref, binding];
+    if (typeof directive === "function") {
+      directive.apply(vm, args);
+    } else if (typeof directive.bind === "function") {
+      directive.bind.apply(vm, args);
     }
-    const args = [ref, binding]
-    if (typeof directive === 'function') {
-      directive.apply(vm, args)
-    } else if (typeof directive.bind === 'function') {
-      directive.bind.apply(vm, args)
-    }
-  } else if (!newData && oldData) { // unbind
+  } else if (!newData && oldData) {
+    // unbind
     binding = {
       name: oldData.name,
       value: oldData.value,
       expression: oldData.expression,
       arg: oldData.arg,
       modifiers: oldData.modifiers
-    }
-    const args = [ref, binding]
-    if (typeof directive.unbind === 'function') {
-      directive.unbind.apply(vm, args)
+    };
+    const args = [ref, binding];
+    if (typeof directive.unbind === "function") {
+      directive.unbind.apply(vm, args);
     }
   }
 }
 
-function isObjectShallowModified (prev, next) {
-  if (prev == null || next == null || typeof prev !== 'object' || typeof next !== 'object') {
-    return prev !== next
+function isObjectShallowModified(prev, next) {
+  if (
+    prev == null ||
+    next == null ||
+    typeof prev !== "object" ||
+    typeof next !== "object"
+  ) {
+    return prev !== next;
   }
-  const keys = Object.keys(prev)
+  const keys = Object.keys(prev);
   if (keys.length !== Object.keys(next).length) {
-    return true
+    return true;
   }
-  let key
+  let key;
   for (let i = keys.length - 1; i >= 0; i--) {
-    key = keys[i]
+    key = keys[i];
     if (next[key] !== prev[key]) {
-      return true
+      return true;
     }
   }
-  return false
+  return false;
 }
 
-export function directive (Component, createElement) {
+export function directive(Component, createElement) {
   return class DirectiveForm extends Component {
-    constructor (props) {
-      super(props)
+    constructor(props) {
+      super(props);
       this.state = {
         formProps: {}
-      }
-      this.ref = null
+      };
+      this.ref = null;
     }
-    directive (newProps, oldProps) {
-      let context
-      let newDirectives = []
-      let oldDirectives = []
+    directive(newProps, oldProps) {
+      let context;
+      let newDirectives = [];
+      let oldDirectives = [];
       if (oldProps) {
-        context = oldProps[COMMON.directive.context]
-        oldDirectives = oldProps[COMMON.directive.name]
+        context = oldProps[COMMON.directive.context];
+        oldDirectives = oldProps[COMMON.directive.name];
       }
       if (newProps) {
-        context = newProps[COMMON.directive.context]
-        newDirectives = newProps[COMMON.directive.name]
+        context = newProps[COMMON.directive.context];
+        newDirectives = newProps[COMMON.directive.name];
       }
-      const newDirectivesClone = newDirectives.slice()
-      const oldDirectivesClone = oldDirectives.slice()
+      const newDirectivesClone = newDirectives.slice();
+      const oldDirectivesClone = oldDirectives.slice();
       if (Array.isArray(newDirectives) && Array.isArray(oldDirectives)) {
         newDirectives.forEach((newDirective, newIndex) => {
           oldDirectives.forEach((oldDirective, oldIndex) => {
             if (newDirective.name === oldDirective.name) {
-              newDirectivesClone.splice(newIndex, 1, undefined)
-              oldDirectivesClone.splice(oldIndex, 1, undefined)
-              triggerDirective(newDirective, oldDirective, context.vm, this.ref) // update
+              newDirectivesClone.splice(newIndex, 1, undefined);
+              oldDirectivesClone.splice(oldIndex, 1, undefined);
+              triggerDirective(
+                newDirective,
+                oldDirective,
+                context.vm,
+                this.ref
+              ); // update
             }
-          })
-        })
-        newDirectivesClone.forEach(v => { // bind
-          v && triggerDirective(v, null, context.vm, this.ref)
-        })
-        oldDirectivesClone.forEach(v => { // unbind
-          v && triggerDirective(null, v, context.vm, this.ref)
-        })
+          });
+        });
+        newDirectivesClone.forEach(v => {
+          // bind
+          v && triggerDirective(v, null, context.vm, this.ref);
+        });
+        oldDirectivesClone.forEach(v => {
+          // unbind
+          v && triggerDirective(null, v, context.vm, this.ref);
+        });
       }
     }
-    handle (props) {
-      const obj = {}
+    handle(props) {
+      const obj = {};
       for (const key in props) {
-        if (key !== COMMON.directive.name &&
+        if (
+          key !== COMMON.directive.name &&
           key !== COMMON.directive.tag &&
-          key !== COMMON.directive.context) {
-          obj[key] = props[key]
+          key !== COMMON.directive.context
+        ) {
+          obj[key] = props[key];
         }
       }
-      const refFn = obj.ref || function () {}
+      const refFn = obj.ref || function() {};
       obj.ref = ref => {
-        this.ref = ref
-        return refFn(ref)
-      }
-      const onChangeFn = obj.onChange || function () {}
+        this.ref = ref;
+        return refFn(ref);
+      };
+      const onChangeFn = obj.onChange || function() {};
       obj.onChange = event => {
         this.setState({
           formProps: Object.assign({}, this.state.formProps, {
             value: event.target.value
           })
-        })
-        return onChangeFn(event)
-      }
+        });
+        return onChangeFn(event);
+      };
       this.setState({
         formProps: obj
-      })
+      });
     }
-    componentWillMount () {
-      this.handle(this.props)
+    componentWillMount() {
+      this.handle(this.props);
     }
-    componentDidMount () {
-      this.directive(this.props)
+    componentDidMount() {
+      this.directive(this.props);
     }
-    componentWillReceiveProps (nextProps) {
-      this.handle(nextProps)
-      this.directive(nextProps, this.props)
+    componentWillReceiveProps(nextProps) {
+      this.handle(nextProps);
+      this.directive(nextProps, this.props);
     }
-    componentWillUnmount () {
-      this.directive(null, this.props)
+    componentWillUnmount() {
+      this.directive(null, this.props);
     }
-    shouldComponentUpdate (nextProps, nextState) {
-      return isObjectShallowModified(this.props, nextProps)
+    shouldComponentUpdate(nextProps, nextState) {
+      return isObjectShallowModified(this.props, nextProps);
     }
-    render () {
-      return createElement(this.props[COMMON.directive.tag], this.state.formProps, this.props.children)
+    render() {
+      return createElement(
+        this.props[COMMON.directive.tag],
+        this.state.formProps,
+        this.props.children
+      );
     }
-  }
+  };
 }
