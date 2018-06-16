19dd1ce7580dc82273cee54c014e14c551a93446
gauravprwl14 <gauravprwl14@gmail.com>
2018-06-16 23:27:10 +0530

    fix the linting error and replace the react-vue with vue-native in the warning console

--- src/platforms/vue-native/runtime/render-helpers/checkKeyCodes.js
+++ src/platforms/vue-native/runtime/render-helpers/checkKeyCodes.js
@@ -1,24 +1,26 @@
 /* @flow */
-import { warn } from 'core/util/index'
+import { warn } from "core/util/index";
 /**
  * Runtime helper for checking keyCodes.
  */
-export function checkKeyCodes (
+export function checkKeyCodes(
   vm: Object,
   eventKeyCode: number,
   key: string,
   builtInAlias: number | Array<number> | void
 ): boolean {
-  let configKeyCodes = {}
+  let configKeyCodes = {};
   try {
-    configKeyCodes = vm.$options._base.config.keyCodes
+    configKeyCodes = vm.$options._base.config.keyCodes;
   } catch (e) {
-    warn('react-vue checkKeyCodes vm.$options._base.config.keyCodes catch error')
+    warn(
+      "vue-native checkKeyCodes vm.$options._base.config.keyCodes catch error"
+    );
   }
-  const keyCodes = configKeyCodes[key] || builtInAlias
+  const keyCodes = configKeyCodes[key] || builtInAlias;
   if (Array.isArray(keyCodes)) {
-    return keyCodes.indexOf(eventKeyCode) === -1
+    return keyCodes.indexOf(eventKeyCode) === -1;
   } else {
-    return keyCodes !== eventKeyCode
+    return keyCodes !== eventKeyCode;
   }
 }
