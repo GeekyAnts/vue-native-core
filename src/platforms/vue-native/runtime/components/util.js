eee30165d8460b9fc5c8cfc2edc15cc56b76bd57
gauravprwl14 <gauravprwl14@gmail.com>
2018-06-16 23:23:37 +0530

    improve the linting error in the utils file of the runtime component

--- src/platforms/vue-native/runtime/components/util.js
+++ src/platforms/vue-native/runtime/components/util.js
@@ -1,8 +1,6 @@
-import {
-  COMMON
-} from 'react-vue/compiler/config'
+import { COMMON } from "react-vue/compiler/config";
 
-export function isObjectShallowModified (prev, next) {
+export function isObjectShallowModified(prev, next) {
   // if (prev == null || next == null || typeof prev !== 'object' || typeof next !== 'object') {
   //   return prev !== next
   // }
@@ -19,34 +17,39 @@ export function isObjectShallowModified (prev, next) {
   // }
   // return false
   if (prev.children !== undefined || next.children !== undefined) {
-    return true
+    return true;
   }
   for (const k in next) {
-    if (typeof next[k] !== 'object') {
+    if (typeof next[k] !== "object") {
       if (next[k] !== prev[k]) {
-        return true
+        return true;
       }
     }
   }
-  return false
+  return false;
 }
 
-const hasOwnProperty = Object.prototype.hasOwnProperty
-export function hasOwn (obj, key) {
-  return hasOwnProperty.call(obj, key)
+const hasOwnProperty = Object.prototype.hasOwnProperty;
+export function hasOwn(obj, key) {
+  return hasOwnProperty.call(obj, key);
 }
 
-export function mergeCssModule (computed, cssModules) {
-  const _computed = Object.create(computed || null)
-  Object.keys(cssModules).forEach(function (key) {
-    var module = cssModules[key]
-    _computed[key] = function () { return module }
-  })
-  return _computed
+export function mergeCssModule(computed, cssModules) {
+  const _computed = Object.create(computed || null);
+  Object.keys(cssModules).forEach(function(key) {
+    var module = cssModules[key];
+    _computed[key] = function() {
+      return module;
+    };
+  });
+  return _computed;
 }
 
-export function pascalCaseTag (tag) {
-  return tag.split('-').map(v => v.replace(/^[a-z]/, s => s.toUpperCase())).join('')
+export function pascalCaseTag(tag) {
+  return tag
+    .split("-")
+    .map(v => v.replace(/^[a-z]/, s => s.toUpperCase()))
+    .join("");
 }
 
 /**
@@ -54,69 +57,70 @@ export function pascalCaseTag (tag) {
  * @param {Object} components
  */
 
-export function handleComponents (components) {
+export function handleComponents(components) {
   for (const k in components) {
     if (hasOwn(components, k)) {
-      components[pascalCaseTag(k)] = components[k]
-      const c = components[k]
+      components[pascalCaseTag(k)] = components[k];
+      const c = components[k];
       if (c.name) {
-        components[pascalCaseTag(c.name)] = components[k]
+        components[pascalCaseTag(c.name)] = components[k];
       }
     }
   }
-  return components
+  return components;
 }
 
-export function handleDirectives (directives) {
-  const obj = {}
+export function handleDirectives(directives) {
+  const obj = {};
   for (const k in directives) {
-    obj[k.toLowerCase().replace(/[^a-z]/g, '')] = directives[k]
+    obj[k.toLowerCase().replace(/[^a-z]/g, "")] = directives[k];
   }
-  return obj
+  return obj;
 }
 
 /**
  * for 'this.$solts'
  * @param {this.props.children} children
  */
-export function getSlots (children) {
-  const slots = {}
+export function getSlots(children) {
+  const slots = {};
   if (children == null) {
-    return slots
+    return slots;
   }
   if (!Array.isArray(children)) {
-    children = [children]
+    children = [children];
   }
-  children = children.filter(v => v != null)
+  children = children.filter(v => v != null);
   children.forEach((v, _i) => {
-    if (typeof v === 'string' || typeof v === 'number' || v === null) {
-      slots.default = slots.default || []
-      slots.default.push(v)
+    if (typeof v === "string" || typeof v === "number" || v === null) {
+      slots.default = slots.default || [];
+      slots.default.push(v);
     } else if (v.type === COMMON.template.type) {
-      slots[v['data-slot']] = slots[v['data-slot']] || []
-      slots[v['data-slot']].push(v.render)
+      slots[v["data-slot"]] = slots[v["data-slot"]] || [];
+      slots[v["data-slot"]].push(v.render);
     } else if (v.props) {
-      const dataSlot = v.props['data-slot']
+      const dataSlot = v.props["data-slot"];
       if (dataSlot == null) {
-        slots.default = slots.default || []
-        slots.default.push(v)
+        slots.default = slots.default || [];
+        slots.default.push(v);
       } else {
-        slots[dataSlot] = slots[dataSlot] || []
-        slots[dataSlot].push(v)
+        slots[dataSlot] = slots[dataSlot] || [];
+        slots[dataSlot].push(v);
       }
     }
-  })
-  return slots
+  });
+  return slots;
 }
 
-export function filterCustomEvent (props) {
-  return Object.keys(props).filter(v => {
-    return v.indexOf(COMMON.customEvent.name) === 0
-  }).map(v => {
-    return {
-      name: v.replace(COMMON.customEvent.name, ''),
-      handle: props[v]
-    }
-  })
+export function filterCustomEvent(props) {
+  return Object.keys(props)
+    .filter(v => {
+      return v.indexOf(COMMON.customEvent.name) === 0;
+    })
+    .map(v => {
+      return {
+        name: v.replace(COMMON.customEvent.name, ""),
+        handle: props[v]
+      };
+    });
 }
-
