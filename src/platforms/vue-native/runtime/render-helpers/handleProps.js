edda4b6db36c0eab44cc87ccd379ade1518be897
gauravprwl14 <gauravprwl14@gmail.com>
2018-06-16 23:34:10 +0530

    fix the linting error in the handleProps

--- src/platforms/vue-native/runtime/render-helpers/handleProps.js
+++ src/platforms/vue-native/runtime/render-helpers/handleProps.js
@@ -1,26 +1,25 @@
-import {
-  HELPER_HEADER
-} from 'react-vue/compiler/constants'
-import propertyMap from 'react-vue/compiler/property/index'
-import {
-  isReservedTag,
-  isUnaryTag
-} from 'react-vue/compiler/util/index'
+import { HELPER_HEADER } from "react-vue/compiler/constants";
+import propertyMap from "react-vue/compiler/property/index";
+import { isReservedTag, isUnaryTag } from "react-vue/compiler/util/index";
 
-export function handleProps (props, tag) {
-  let handledProps = {}
-  if (typeof tag === 'string' && isReservedTag(tag)) {
+export function handleProps(props, tag) {
+  let handledProps = {};
+  if (typeof tag === "string" && isReservedTag(tag)) {
     for (const key in props) {
-      const prop = propertyMap[key.toLowerCase()]
-      if (prop && props[key] !== undefined && key.indexOf(HELPER_HEADER) !== 0) {
-        handledProps[prop] = props[key]
+      const prop = propertyMap[key.toLowerCase()];
+      if (
+        prop &&
+        props[key] !== undefined &&
+        key.indexOf(HELPER_HEADER) !== 0
+      ) {
+        handledProps[prop] = props[key];
       }
     }
   } else {
-    handledProps = props
+    handledProps = props;
   }
   if (isUnaryTag(tag)) {
-    delete handledProps.children
+    delete handledProps.children;
   }
-  return handledProps
+  return handledProps;
 }
