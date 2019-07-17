782a0048f4393e9ea161af5c8ee4d4a45e36471a
Rishabh Karnad <rishabhk@geekyants.com>
2019-07-17 16:16:27 +0530

    moved the contents of packages/vue-native-scripts to src

--- packages/vue-native-scripts/index.js
+++ packages/vue-native-scripts/index.js
@@ -1,3 +1,1 @@
-// module.exports = require('./src/bin/vue-native-script.js');
-module.exports.compileVueToRn = require('./src/scripts/compiler.js');
-module.exports.transform = require('./src/scripts/transformerPlugin.js');
+module.exports = require('./build.js')
